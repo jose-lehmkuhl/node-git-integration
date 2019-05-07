e96838baa24cd13efe087a45b599c80ec0f20f95
Jose Lehmkuhl <josevicentekoerich@gmail.com>
2019-05-07 19:36:11 -0300

    changed default port to remove conflict

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,20 @@
-"use strict";
+'use strict';
 
 let gulp = require('gulp');
 let jshint = require('gulp-jshint');
 
-gulp.task('default',['jshint','test','serve']);
+gulp.task('default', ['jshint', 'test', 'serve']);
 
-gulp.task('jshint',()=>{
-	return gulp.src('./*.js')
-		.pipe(jshint())
-		.pipe(jshint.reporter('default'));
-})
-.task('test',()=>{
-	require('./test.js');
-})
-.task('serve',()=>{
-	require('./main.js');
-});
+gulp
+  .task('jshint', () => {
+    return gulp
+      .src('./*.js')
+      .pipe(jshint())
+      .pipe(jshint.reporter('default'));
+  })
+  .task('test', () => {
+    require('./test.js');
+  })
+  .task('serve', () => {
+    require('./main');
+  });
